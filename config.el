1510085f21ff40a1c31a77f76390978a1fd1b50f
Yu Liang <luy70@psu.edu>
2021-10-31 14:31:28 -0400

    Init working config

---
+++ config.el
@@ -0,0 +1,113 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Yu Liang"
+      user-mail-address "luy70@psu.edu")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "SF MONO" :size 14 :weight 'Regular)
+       doom-variable-pitch-font (font-spec :family "SF MONO" :size 14))
+
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; Set up the hybrid line numbers display
+(setq display-line-numbers-type 'relative) 
+(global-display-line-numbers-mode)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Python conda env setup
+(require 'conda)
+;; if you want interactive shell support, include:
+(conda-env-initialize-interactive-shells)
+;; if you want eshell support, include:
+(conda-env-initialize-eshell)
+;; if you want auto-activation (see below for details), include:
+(setq-default conda-env-autoactivate-mode t)
+(setq conda-anaconda-home (expand-file-name "/Users/sly/opt/anaconda3"))
+(setq conda-env-home-directory (expand-file-name "/Users/sly/opt/anaconda3"))
+
+;;;; Set up the C/C++ language server
+ (setq lsp-clients-clangd-args '("-j=3"
+                                 "--background-index"
+                                 "--clang-tidy"
+                                 "--completion-style=detailed"
+                                 "--header-insertion=never"
+                                 "--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+
+
+;; (after! ccls
+;;   (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
+;;   (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
+
+
+;; (after! lsp-mode
+;;   :hook ((prog-mode . lsp-deferred))
+;;   :commands (lsp lsp-deferred)
+;;   :config
+;;   (progn
+;;     (lsp-register-client
+;;      (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
+;;                       :major-modes '(c-mode c++-mode)
+;;                       :remote? t
+;;                       :server-id 'clangd-remote))))
+
+;; (require 'lsp-mode)
+;;  (lsp-register-client
+;;      (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
+;;                       :major-modes '(c-mode c++-mode)
+;;                       :remote? t
+;;                       :priority 2
+;;                       :server-id 'clangd-remote))
+
+(setq lsp-log-io t)
+
+(setq lsp-headerline-breadcrumb-enable t)
+(setq lsp-ui-doc-enable nil)
+
+(setq toggle-debug-on-quit t)
+
+;; (set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))
+;; (set-eglot-client! 'cc-mode '("ccls" "--init={\"index\": {\"threads\": 3}}"))
