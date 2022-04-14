e54a32c4cca021ba16fdd314fb1a6c2d98aa82c6
Yu Liang <luy70@psu.edu>
2022-04-14 13:17:26 -0400

    Added function to copy current opened filename to clipboard.

--- config.el
+++ config.el
@@ -119,3 +119,13 @@
 (after! ccls
   (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
   (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
+
+(defun er-copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
